9cad89c322c3547ee562e9b06478170860dc34ef
Ryan Flach <ryanflach@users.noreply.github.com>
2017-09-17 18:05:00 -0600

    Complete 'Maps' section

--- map/main.go
+++ map/main.go
@@ -17,3 +17,18 @@ func printMap(c map[string]string) {
 		fmt.Println("Hex code for", color, "is", hex)
 	}
 }
+
+// Map vs Struct
+// Map:
+// - All keys must be the same type
+// - All values must be the same type
+// - Keys are indexed and can be iterated over
+// - Generally used to represent a collection of related properties
+// - Don't need to know all the keys at compile time
+// - Reference Type (passed as a reference, rather than a copy of the value)
+// Struct:
+// - Values can be of different type
+// - Keys don't support indexing
+// - You need to know all the different fields at compile time
+// - Generally used to represent a 'thing' with a lot of different properties
+// - Value Type
